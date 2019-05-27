725fda4c7f75a4e6bf7a6474fcc1e99444b19d67
JiaMing <ty10410205@gmail.com>
2019-05-27 20:05:12 +0800

    change .c

--- A20.c
+++ A20.c
@@ -1,3 +1,11 @@
+/*
+作業20:請撰寫一個程式，
+用巨集定義數學函式
+f(a,b,c) = (a+b+c)(a-b-c)，
+並由鍵盤輸入a、b、c值。 
+*呂家明 3a713229
+*108/5/25
+*/ 
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -6,12 +14,14 @@
 int main(void){
 	
 	int a,b,c;
+	
 	printf("請輸入a,b,c:");
+	
 	scanf("%d", &a);
 	scanf("%d", &b);
 	scanf("%d", &c);
 
-	printf("result:%d",f);
+	printf("\nresult:%d\n",f);
 	
 	system("pause");
 	return 0;
